f79484cbff3a5d9e305960ad172ed91f01c63999
ruioliveira02 <rpoliveira02@gmail.com>
2022-04-06 21:07:30 +0100

    Initial update. Missing running jar

--- Vagrantfile
+++ Vagrantfile
@@ -16,6 +16,7 @@ Vagrant.configure("2") do |config|
   config.vm.provision :shell, path: "bootstrap.sh"
   config.vm.network "private_network", ip: "192.168.56.21"
   config.vm.synced_folder "./", "/vagrant", type: "rsync", rsync__auto: true, rsync__exclude: ['./node_modules*']
+  config.vm.synced_folder "../", "/solver", type: "rsync", rsync__auto: true, rsync__exclude: ['./node_modules*']
   # Disable automatic box update checking. If you disable this, then
   # boxes will only be checked for updates when the user runs
   # `vagrant box outdated`. This is not recommended.
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+#Dependencies
 apt-get update
-apt-get install -y php7.4-mysql php7.4-xml php7.4-gd php7.4-mbstring php7.4-zip mysql-server composer npm 
+apt-get install -y php7.4-mysql php7.4-xml php7.4-gd php7.4-mbstring php7.4-zip mysql-server composer npm openjdk-17-jre-headless
+
+#Install maven manually
+mkdir maven
+cd /maven
+wget -P https://dlcdn.apache.org/maven/maven-3/3.8.5/binaries/apache-maven-3.8.5-bin.tar.gz /maven
+tar xzvf apache-maven-3.8.5-bin.tar.gz
+export PATH=/opt/apache-maven-3.8.5/bin:$PATH
+
 cd /vagrant
 rm package-lock.json
 mysql -u root -p -e 'CREATE DATABASE swap;'
